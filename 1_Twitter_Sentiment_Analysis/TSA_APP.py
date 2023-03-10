6da6f643b13f2cfaf8926ae142267e41d92651db
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-10 10:47:26 +0530

    Update TSA_APP.py

--- 1_Twitter_Sentiment_Analysis/TSA_APP.py
+++ 1_Twitter_Sentiment_Analysis/TSA_APP.py
@@ -10,9 +10,9 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 # read CSV file and create independent and dependent variables
 # DATASETS
 Twitter_DF1 = pd.read_csv(r'https://raw.githubusercontent.com/tulasinnd/Social-Metric-Insight-Projects/main/1_Twitter_Sentiment_Analysis/DATASETS/TWITTER_SENTIMENT_ANALYSIS_PROCESSED1.csv')
-Twitter_DF2 = pd.read_csv(r'DATASETS/TWITTER_SENTIMENT_ANALYSIS_PROCESSED2.csv')
-Twitter_DF3 = pd.read_csv(r'DATASETS/TWITTER_SENTIMENT_ANALYSIS_PROCESSED3.csv')
-Twitter_DF4 = pd.read_csv(r'DATASETS/TWITTER_SENTIMENT_ANALYSIS_PROCESSED4.csv')
+Twitter_DF2 = pd.read_csv(r'https://raw.githubusercontent.com/tulasinnd/Social-Metric-Insight-Projects/main/1_Twitter_Sentiment_Analysis/DATASETS/TWITTER_SENTIMENT_ANALYSIS_PROCESSED2.csv')
+Twitter_DF3 = pd.read_csv(r'https://raw.githubusercontent.com/tulasinnd/Social-Metric-Insight-Projects/main/1_Twitter_Sentiment_Analysis/DATASETS/TWITTER_SENTIMENT_ANALYSIS_PROCESSED3.csv')
+Twitter_DF4 = pd.read_csv(r'https://raw.githubusercontent.com/tulasinnd/Social-Metric-Insight-Projects/main/1_Twitter_Sentiment_Analysis/DATASETS/TWITTER_SENTIMENT_ANALYSIS_PROCESSED4.csv')
 Twitter_DF = pd.concat([Twitter_DF1,Twitter_DF2,Twitter_DF3,Twitter_DF4], axis=0)
 
 # recognise independent and dependent features
