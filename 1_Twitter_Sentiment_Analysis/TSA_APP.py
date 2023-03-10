df9b68faee0a8c33d129c084b42f4b365322499e
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-10 11:44:52 +0530

    Update TSA_APP.py

--- 1_Twitter_Sentiment_Analysis/TSA_APP.py
+++ 1_Twitter_Sentiment_Analysis/TSA_APP.py
@@ -64,11 +64,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
-# read CSV file and create independent and dependent variables. Here I have taken a sample of entire dataset because of spaces issues
-# you can find complete dataset from https://www.kaggle.com/datasets/kazanova/sentiment140
+# read CSV file and create independent and dependent variables
 Twitter_DF = pd.read_csv(r'https://raw.githubusercontent.com/tulasinnd/Social-Metric-Insight-Projects/main/1_Twitter_Sentiment_Analysis/DATASETS/TWITTER_SENTIMENT_ANALYSIS_PROCESSED_2000.csv')
+
+# recognise independent and dependent features
 Independent_var = Twitter_DF['text'] 
 Dependent_var = Twitter_DF['final_target']
 
@@ -100,11 +101,16 @@ if user_input:
     sentiment = "Positive" if result == 2 else "Negative"
     st.write(f"Predicted sentiment: {sentiment}")
 
-    # print f1_score
+    # print accuracy, precision, recall, and confusion matrix
     accuracy = accuracy_score(predictions, DV_test)
-    precision, recall, f1_score, support = precision_recall_fscore_support(predictions, DV_test, average='binary')
+    precision = precision_score(predictions, DV_test, average='weighted')
+    recall = recall_score(predictions, DV_test, average='weighted')
+    f1 = f1_score(predictions, DV_test, average='weighted')
     cm = confusion_matrix(predictions, DV_test)
-    st.write('##### Model Performance')
-    st.write(f"F1 score: {f1_score:.2f}")
+    st.write("PERFORMANCE METRICS")
+    st.write(f"Accuracy: {accuracy:.2f}")
+    st.write(f"Precision: {precision:.2f}")
+    st.write(f"Recall: {recall:.2f}")
+    st.write(f"F1 score: {f1:.2f}")
+    st.write("Confusion matrix:")
     st.write(cm)
-
